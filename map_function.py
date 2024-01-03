d69c432a8446c15ff1146713152be0dd750198d9
lessan-cyber <121871770+lessan-cyber@users.noreply.github.com>
2024-01-03 17:34:54 +0100

    Update map_function.py

--- map_function.py
+++ map_function.py
@@ -2,3 +2,8 @@ notes = [10, 11, 12, 15, 12, 13]
 for i in range(len(notes)):
     notes[i] += 2
 print(notes)
+
+# faire la meme chose avec map
+marks = [10, 11, 12, 15, 12, 13]
+marksTwo = map(lambda x: x+2, marks)
+print(list(marksTwo))
