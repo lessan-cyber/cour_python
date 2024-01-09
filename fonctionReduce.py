a6441cebede4761eab05535a1bf14c971f32e748
lessan-cyber <121871770+lessan-cyber@users.noreply.github.com>
2024-01-09 14:11:02 +0100

    Update fonctionReduce.py

--- fonctionReduce.py
+++ fonctionReduce.py
@@ -7,6 +7,8 @@ resultMultiplication = reduce(lambda x, y: x * y, my_liste)  # affiche la multip
 resultatAddition = reduce(lambda x, y: x + y, my_liste)  # affiche la somme de tous les éléments de la liste
 print(resultMultiplication)
 
+
+
 my_liste2 = [1 , "lessan",  2, 3, 4, "jean", "afi", 5, "christian", 6, 7, 8, 9]
 resultatEntier = filter(lambda x: type(x) == int, my_liste2)  # affiche la liste des entiers
 resutatPaire = filter(lambda x: x % 2 == 0, my_liste)  # affiche la liste des entiers pairs
