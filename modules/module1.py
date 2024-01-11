6d81b2aa4ad1fe61082d979323d1f81afbfdf86c
lessan-cyber <121871770+lessan-cyber@users.noreply.github.com>
2024-01-11 13:16:37 +0100

    update modules folder

---
+++ modules/module1.py
@@ -0,0 +1,33 @@
+from functools import reduce
+def average(liste):
+    # verifier si l element est un entier d abord
+    listeInt = list(filter(lambda x: type(x) == int, liste))
+    print(listeInt)
+    # moyenne = listeInt.reduce(lambda x, y: x + y, listeInt) / len(listeInt)
+    # # on peut aussi utiliser la fonction sum en verifiant que tu les element sont
+    # return moyenne
+def nombrePaire(liste):
+    listePaire = liste.filter(lambda x: x % 2 == 0, liste)
+    return listePaire
+
+def multipy(liste):
+    listeMultiplication = liste.reduce(lambda x, y: x * y, liste)
+    return listeMultiplication
+
+# def listeDeVoyelle(c):
+#     return list(filter(lambda x: x in "aeiouy", c))
+#
+# message = "Bonjour les gens"
+# print(listeDeVoyelle(message))
+#
+# def upper_caracter(message):
+#     return list(map(lambda x: x.upper(), message))
+#
+# print(upper_caracter(message))
+
+def upperTheFirstCaracter(mot):
+    print(mot[:3])
+    return mot[0].upper() + mot[1:]
+
+my = "bonjour"
+print(upperTheFirstCaracter(my))
--- modules/module2.py
+++ modules/module2.py
@@ -1,30 +1,5 @@
-def average(liste):
-    # verifier si l element est un entier d abord
-    listeInt = list(filter(lambda x: type(x) == int, liste))
-    moyenne = liste.reduce(lambda x, y: x + y, listeInt) / len(listeInt)
-    # on peut aussi utiliser la fonction sum en verifiant que tu les element sont
-    return moyenne
-def nombrePaire(liste):
-    listePaire = liste.filter(lambda x: x % 2 == 0, liste)
-    return listePaire
+from module1 import *
 
-def multipy(liste):
-    listeMultiplication = liste.reduce(lambda x, y: x * y, liste)
-    return listeMultiplication
+mes_notes = [15, "bishop", 12, 13, 10, "abalo"]
 
-# def listeDeVoyelle(c):
-#     return list(filter(lambda x: x in "aeiouy", c))
-#
-# message = "Bonjour les gens"
-# print(listeDeVoyelle(message))
-#
-# def upper_caracter(message):
-#     return list(map(lambda x: x.upper(), message))
-#
-# print(upper_caracter(message))
-
-def upperTheFirstCaracter(mot):
-    return mot[0].upper() + mot[1:]
-
-my = "bonjour"
-print(upperTheFirstCaracter(my))
+print(average(mes_notes))
