c530bbfbc4b799853fc31420f6789eb57b4030ed
lessan-cyber <121871770+lessan-cyber@users.noreply.github.com>
2024-02-03 18:41:19 +0000

    Create exam.py

---
+++ exam.py
@@ -0,0 +1,47 @@
+a = 0
+b = 10
+# for i in range(a, b):
+#     print(i)
+
+# while a < b:
+#     print(a)
+#     a += 1
+
+# for j in range(b, a, -1):
+#     if j % 2 != 0:
+#         print(j)
+
+while b > 0:
+    if b % 2 != 0:
+        print(b)
+    b -= 1
+
+
+# for y in range(0, 15, -3):
+#     print(y)
+
+# for i in range(1, 11):
+#     print(i)
+#     if i == 5:
+#         break
+#
+# a = 5
+# def compute(b, c):
+#     a = b * c
+#     return a
+# compute(8, 5)
+# print(a)
+
+
+
+# liste = [17, 38, 10, 25, 72]
+# liste.append(12)
+# print(liste)
+# print(liste.index(17))
+# liste.remove(38)
+# print(liste)
+# liste.pop(3)
+# print(liste)
+# print(liste[1:3])
+# print(liste[:2])
+# print(liste[2:])
